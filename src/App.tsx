b1a35bc1b67a2c066822c3648ad51a88bef1e244
Kenan Maslesa <kenan@logicflow.ai>
2022-11-20 01:16:31 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import TodoList from "./todo/components/TodoList";
 
 const App = () => {
   useEffect(() => {
-    document.title = "React Todos Template";
+    document.title = "React Todos Redux";
   }, []);
 
   return (
     <>
-      <h1 className="title">Todos</h1>
+      <h1 className="title">Todos Redux</h1>
       <section className="todo-section">
         <TodoAdd />
         <TodoList />
